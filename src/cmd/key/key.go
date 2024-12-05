beb93c822d4efe8acf8127821dbc1a350d7b5e9e
Mikhail <kami@slink.ws>
2024-12-05 20:23:46 +1000

    encode generated key to base64

--- src/cmd/key/key.go
+++ src/cmd/key/key.go
@@ -1,6 +1,7 @@
 package key
 
 import (
+	"encoding/base64"
 	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -19,6 +20,9 @@ var keygen = &cobra.Command{
 		if err != nil {
 			fmt.Printf("\nerror: %s\n\n", err)
 		} else {
+			if viper.GetBool(viperBase64EncodeParam) {
+				key = base64.StdEncoding.EncodeToString([]byte(key))
+			}
 			fmt.Printf("\n%s\n\n", key)
 			if viper.GetBool(viperKeySaveParam) {
 				util.SaveKeyConfig(key)
@@ -28,15 +32,19 @@ var keygen = &cobra.Command{
 }
 
 const (
-	cobraKeyLengthFlag  = "length"
-	viperKeyLengthParam = "keygen.length"
-	cobraKeyAlgoFlag    = "algo"
-	viperKeyAlgoParam   = "keygen.algo"
-	cobraKeySaveFlag    = "save"
-	viperKeySaveParam   = "keygen.save"
+	cobraBase64EncodeFlag  = "base64"
+	viperBase64EncodeParam = "keygen.base64"
+	cobraKeyLengthFlag     = "length"
+	viperKeyLengthParam    = "keygen.length"
+	cobraKeyAlgoFlag       = "algo"
+	viperKeyAlgoParam      = "keygen.algo"
+	cobraKeySaveFlag       = "save"
+	viperKeySaveParam      = "keygen.save"
 )
 
 func init() {
+	keygen.Flags().BoolP(cobraBase64EncodeFlag, "b", false, "encode result to base64")
+	_ = viper.BindPFlag(viperBase64EncodeParam, keygen.Flags().Lookup(cobraBase64EncodeFlag))
 	keygen.Flags().IntP(cobraKeyLengthFlag, "l", 0, "key length")
 	_ = viper.BindPFlag(viperKeyLengthParam, keygen.Flags().Lookup(cobraKeyLengthFlag))
 	keygen.Flags().StringP(cobraKeyAlgoFlag, "a", "", "signature algorithm; supported values: hs256, hs384, hs512, rs256, rs384, rs512, es256, es384, es512, ps256, ps384, ps512")
